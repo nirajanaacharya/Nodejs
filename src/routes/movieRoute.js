c16a74d4b51c1fe9c2cb06bd4e2d2ba7f59f58a8
nirajanaacharya <nirajanlegendkilker15@gmail.com>
2026-04-28 21:27:13 +0545

    database connectivity

--- src/routes/movieRoute.js
+++ src/routes/movieRoute.js
@@ -3,10 +3,19 @@ import express from "express"
 const router = express.Router();
 
 router.get("/",(req ,res)=>{
-    res.json({Message: "Suman is Gayda."})
+    res.json({Message: "Suman get that thing for me."})
 });
+
+router.post("/",(req ,res)=>{
+    res.json({Message: "Suman post this video."})
+});
+
+router.put("/",(req ,res)=>{
+    res.json({Message: "Suman just put it down."})
+});
+
 router.delete("/",(req ,res)=>{
-    res.json({Message: "SUman is deleted from this world"})
+    res.json({Message: "Suman is deleted from this world."})
 });
 
 export default router;
